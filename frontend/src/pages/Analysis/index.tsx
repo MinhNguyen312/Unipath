e980e57d2da394c54c57e1c323dc45d2324fbdf8
nhungnguyen1503 <nhung1899@gmail.com>
2025-05-03 00:23:30 +0700

    add tableau dashboard to analytic page

--- frontend/src/pages/Analysis/index.tsx
+++ frontend/src/pages/Analysis/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Layout } from 'antd';
-import SchoolComparison from '../../components/SchoolCompare/SchoolCompare';
 import './styles.css';
-import ChatbotWidget from '../../components/Chatbot/ChatbotWidget';
 const { Content } = Layout;
+import TableauEmbed from '../../components/Tableau/TableauEmbed';
+
 
 const Finder: React.FC = () => {
   return (
     <Layout>
-      <Content className="finder-content">
-        <SchoolComparison />
-        <ChatbotWidget />
+      <Content className="finder-content" style={{ backgroundColor: 'white' }}>
+        <TableauEmbed />
       </Content>
     </Layout>
   );
