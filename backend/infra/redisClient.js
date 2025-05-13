482478cfd552e941d7d2a02e7129cd85f19afada
nhungnguyen1503 <nhung1899@gmail.com>
2025-05-14 05:14:46 +0700

    add redis for backend

---
+++ backend/infra/redisClient.js
@@ -0,0 +1,16 @@
+const redis = require("redis");
+
+const redisClient = redis.createClient({
+  url: `${process.env.REDIS_URL}`
+});
+
+redisClient.on("error", (err) => {
+  console.error("Redis error:", err);
+});
+
+(async () => {
+  await redisClient.connect();
+  console.log("Connected to Redis");
+})();
+
+module.exports = redisClient;
--- frontend/src/hooks/useCompareStore.ts
+++ frontend/src/hooks/useCompareStore.ts
@@ -1,10 +1,10 @@
 export type MajorInfo = {
-    university: string;
-    major: string;
-    city: string;
-    fee: string;
-    examBlocks: string[];
-    scores: { year: number; score: number }[];
-  };
-  
+  university: string;
+  major: string;
+  city: string;
+  fee: string;
+  examBlocks: string[];
+  scores: { year: number; score: number }[];
+};
+
 export const majorCompareCache: Record<string, MajorInfo> = {};
